ca13c7d533930c20cd896963a688335788350b98
PierrePh@rel <edo.tensei095@gmail.com>
2020-11-06 19:16:47 +0100

    discord version in shell script

--- discord-tray-icon/fix-tray.sh
+++ discord-tray-icon/fix-tray.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-asar e $HOME/.config/discord/0.0.12/modules/discord_desktop_core/core.asar $HOME/.config/discord/0.0.12/modules/discord_desktop_core/core
+asar e $HOME/.config/discord/X.X.XX/modules/discord_desktop_core/core.asar $HOME/.config/discord/X.X.XX/modules/discord_desktop_core/core
 
-rsvg-convert -w 24 -h 24 -f png -a ./svg/tray.svg > $HOME/.config/discord/0.0.12/modules/discord_desktop_core/core/app/images/systemtray/linux/tray.png
+rsvg-convert -w 24 -h 24 -f png -a ./svg/tray.svg > $HOME/.config/discord/X.X.XX/modules/discord_desktop_core/core/app/images/systemtray/linux/tray.png
 
-rsvg-convert -w 24 -h 24 -f png -a ./svg/tray-unread.svg >  $HOME/.config/discord/0.0.12/modules/discord_desktop_core/core/app/images/systemtray/linux/tray-unread.png
+rsvg-convert -w 24 -h 24 -f png -a ./svg/tray-unread.svg >  $HOME/.config/discord/X.X.XX/modules/discord_desktop_core/core/app/images/systemtray/linux/tray-unread.png
 
-asar p $HOME/.config/discord/0.0.12/modules/discord_desktop_core/core $HOME/.config/discord/0.0.12/modules/discord_desktop_core/core.asar
+asar p $HOME/.config/discord/X.X.XX/modules/discord_desktop_core/core $HOME/.config/discord/X.X.XX/modules/discord_desktop_core/core.asar
 
-rm -rfv $HOME/.config/discord/0.0.12/modules/discord_desktop_core/core
+rm -rfv $HOME/.config/discord/X.X.XX/modules/discord_desktop_core/core
